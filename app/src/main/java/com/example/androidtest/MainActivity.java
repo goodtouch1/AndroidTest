281447172ebbdfe91ed13580b3346ce5ade996fe
EnesUCA <79279212+EnesUCA@users.noreply.github.com>
2021-06-25 01:06:38 +0300

    Ledlerin durum nesnesini olusturduk

--- app/src/main/java/com/example/androidtest/MainActivity.java
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -4,7 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+
+    final DatabaseReference ledstatus1 = myRef.child("kirmizi").child("status");
+
+    final DatabaseReference ledstatus2 = myRef.child("sari").child("status");
+
+    final DatabaseReference ledstatus3 = myRef.child("yesil").child("status");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
