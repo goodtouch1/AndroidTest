4e17070a89faa5cf3611d938e149c80a2e8c40b1
EnesUCA <79279212+EnesUCA@users.noreply.github.com>
2021-06-25 21:30:44 +0300

    Ses dosyası eklenmesi nesnesi tanımlandı

--- app/src/main/java/com/example/androidtest/MainActivity.java
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.androidtest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
     String durum;
+    TextView textView1;
+    MediaPlayer sesliuyarimesaji;
 
     final DatabaseReference ledstatus1 = myRef.child("kirmizi").child("status");
 
@@ -27,13 +31,14 @@ public class MainActivity extends AppCompatActivity {
     final DatabaseReference ledstatus3 = myRef.child("yesil").child("status");
 
 
-    TextView textView1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView1 = (TextView)findViewById(R.id.text);
+        MediaPlayer sesliuyari = MediaPlayer.create(getApplicationContext(), R.raw.ses_dosyasi);
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
